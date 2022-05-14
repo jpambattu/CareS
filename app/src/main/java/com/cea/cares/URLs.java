7db2a86c75d0ae7e0407ab4f76a8d228b61e6ddf
Jpambattu <josephpiousambattu@gmail.com>
2022-05-14 22:50:13 +0530

    Added files

---
+++ app/src/main/java/com/cea/cares/URLs.java
@@ -0,0 +1,8 @@
+package com.cea.cares;
+
+public class URLs {
+    private static final String ROOT_URL = "http://10.0.2.2/project/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
